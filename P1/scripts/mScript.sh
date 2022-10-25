1b7d3642276e8efbbeb0d282f050bda01b57e159
ybenbrai <benbraitit@gmail.com>
2022-10-25 14:51:24 +0100

    integrated all in vagrant and switched to centos

--- P1/scripts/mScript.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo "#######   Installing k3s    #######"
-export INSTALL_K3S_EXEC="--write-kubeconfig-mode 644 --bind-address=192.168.42.110 --node-ip=192.168.42.110 --advertise-address=192.168.42.110"
-curl -sfL https://get.k3s.io | sh -
-sudo cp /var/lib/rancher/k3s/server/token /shared_folder/
--- P1/scripts/wScript.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo "#######   Installing k3s <*>agent<*>    #######"
-export INSTALL_K3S_EXEC="agent --server https://192.168.42.110:6443 --token $(sudo cat /shared_folder/token) --node-ip=192.168.42.111"
-curl -sfL https://get.k3s.io | sh -
