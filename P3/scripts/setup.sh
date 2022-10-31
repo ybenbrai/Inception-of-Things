b52ef8a29fd5c1867aed24fb09de2fff9be76619
Youness Benbraitit <benbraitit@gmail.com>
2022-10-31 22:15:23 +0100

    dom

--- P3/scripts/setup.sh
+++ P3/scripts/setup.sh
@@ -10,7 +10,7 @@ sudo apt-get upgrade -y
 # preparing to install docker src: https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-20-04
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
 
 
 # installing docker
